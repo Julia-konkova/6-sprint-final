a1faa805b31d9c6499660d418b242285449a1fb3
Julia-konkova <iulia.kurleiko@yandex.ru>
2025-05-26 12:25:50 +0300

    6-sprint final

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,70 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func HtmlHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	htmlData, err := os.ReadFile("../6-sprint-final/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(htmlData)
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if err := r.ParseMultipartForm(10 << 20); err != nil { // 10 MB max
+		http.Error(w, fmt.Sprintf("Failed to parse form: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	file, header, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to get file: %v", err), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to read file: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	result := service.ConvertText(string(fileBytes))
+
+	fileName := fmt.Sprintf("%s%s", time.Now().UTC().String(), filepath.Ext(header.Filename))
+	outputPath := fileName
+
+	if err := os.WriteFile(outputPath, []byte(result), 0755); err != nil {
+		http.Error(w, fmt.Sprintf("Failed to save file: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(result))
+
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,30 @@
 package service
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func ConvertText(mes string) string {
+	if len(mes) == 0 {
+		err := errors.New("File empty")
+		return fmt.Sprintf("Error: %v\n", err)
+	}
+	russianAlphabet := "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+	symbolMorse := "-."
+
+	if strings.ContainsAny(mes, russianAlphabet) {
+		result := morse.ToMorse(mes)
+		return result
+	}
+	if strings.ContainsAny(mes, symbolMorse) {
+		result := morse.ToText(mes)
+		return result
+	}
+
+	err := errors.New("Error: invalid type text")
+	return fmt.Sprintf("Error: %v\n", err)
+}
